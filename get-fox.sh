be1f89c8b1fd13e1a61866680dd8240c5c46260b
Rax <61168967+Raxon24@users.noreply.github.com>
2023-03-08 18:56:58 -0500

    Update get-fox.sh

--- get-fox.sh
+++ get-fox.sh
@@ -2,20 +2,16 @@
 
 sudo apt update & sudo apt upgrade
 
-sudo apt install wget nano curl
+#  Install needed PKG
+
+sudo apt install -y wget unzip nano curl ca-certificates gnupg lsb-release dirmngr gnupg gnupg-l10n gnupg-utils gpg gpg-agent gpg-wks-client
+  gpg-wks-server gpgconf gpgsm libassuan0 libksba8 libnpth0 pinentry-curses dbus-user-session pinentry-gnome3 tor parcimonie xloadimage scdaemon
+  pinentry-doc
 
 # Debian Bullseye 11 (stable)
 # remove all Docker install
 
 sudo apt-get remove docker docker-engine docker.io containerd runc
-
-#  Install needed PKG
-
-sudo apt-get install \
-    ca-certificates \
-    curl \
-    gnupg \
-    lsb-release
 
 sudo mkdir -m 0755 -p /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
@@ -31,7 +27,7 @@ sudo apt-get update
 sudo chmod a+r /etc/apt/keyrings/docker.gpg
 sudo apt-get update
 
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
 sudo docker run hello-world
 
