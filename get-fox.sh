db5c4b75fa8c29c0df02b4306ebe4a135bbd922c
Rax <61168967+Raxon24@users.noreply.github.com>
2023-03-09 09:29:44 -0500

    Update get-fox.sh

--- get-fox.sh
+++ get-fox.sh
@@ -47,13 +47,13 @@ sudo docker compose up -d
 
 # Install frented
 
-sudo docker compose exec frontend-build yarn bundle:release
+#sudo docker compose exec frontend-build yarn bundle:release
 
-if [[ -d ./source/frontend_web/app/dist ]] 
-then
-    rm -rf ./public_html/storage/app/web
-    cp -rf ./source/frontend_web/app/dist ./public_html/storage/app/web
-fi
+#if [[ -d ./source/frontend_web/app/dist ]] 
+#then
+#    rm -rf ./public_html/storage/app/web
+#    cp -rf ./source/frontend_web/app/dist ./public_html/storage/app/web
+#fi
 
 # Final Info
 
