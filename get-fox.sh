6b4aaf7ad7984c772eaf8794f4c9a01d409d39cc
Rax <61168967+Raxon24@users.noreply.github.com>
2023-03-11 09:35:07 -0500

    Update get-fox.sh

--- get-fox.sh
+++ get-fox.sh
@@ -45,15 +45,6 @@ fi
 
 sudo docker compose up -d
 
-# Install frented
-
-#sudo docker compose exec frontend-build yarn bundle:release
-
-if [[ -d ./source/frontend_web/app/dist ]] 
-then
-    rm -rf ./public_html/storage/app/web
-    cp -rf ./source/frontend_web/app/dist ./public_html/storage/app/web
-fi
 
 # Install portainer
 
